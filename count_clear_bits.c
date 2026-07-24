ec0a6307b0571f16b816e68d7cc9182fc318ce1c
Musafar <musafarvalayankaden@gmail.com>
2026-07-24 12:09:51 +0530

    Add files via upload

---
+++ count_clear_bits.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+unsigned int num,set,t1=1,count=0;
+printf("enter the num\n");
+scanf("%d",&num);
+
+//input // 0011 0010 //50
+//output //3
+int i;
+for(i=0;i<32;i++)
+  {
+   num>>i & 1 ?0:count++;
+  }  
+printf("the number of set bits:%d\n",count);
+
+return 0;
+}
